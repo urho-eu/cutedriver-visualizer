6f7ed9e8968b2817d0a0da80efc790ef46bc7b98
Ferenc Szekely <ferenc.szekely@gmail.com>
2017-08-03 18:18:07 +0300

    cosmetics

--- rakefile.rb
+++ rakefile.rb
@@ -1,48 +1,46 @@
 ############################################################################
-## 
-## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
-## All rights reserved. 
-## Contact: Nokia Corporation (testabilitydriver@nokia.com) 
-## 
+##
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+## All rights reserved.
+## Contact: Nokia Corporation (testabilitydriver@nokia.com)
+##
 ## This file is part of Testability Driver Qt Agent
-## 
-## If you have questions regarding the use of this file, please contact 
-## Nokia at testabilitydriver@nokia.com . 
-## 
-## This library is free software; you can redistribute it and/or 
-## modify it under the terms of the GNU Lesser General Public 
-## License version 2.1 as published by the Free Software Foundation 
-## and appearing in the file LICENSE.LGPL included in the packaging 
-## of this file. 
-## 
+##
+## If you have questions regarding the use of this file, please contact
+## Nokia at testabilitydriver@nokia.com .
+##
+## This library is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Lesser General Public
+## License version 2.1 as published by the Free Software Foundation
+## and appearing in the file LICENSE.LGPL included in the packaging
+## of this file.
+##
 ############################################################################
-
 
 desc "Task for testing visualizer"
 task :test do
-   begin
-     Dir.chdir("test")
-     result=system( "cucumber features -f TDriverReport::CucumberReporter" )
-     raise "Feature tests failed" if (result != true) or ($? != 0) 
-   ensure    
-     Dir.chdir("..")	 
-	if ENV['CC_BUILD_ARTIFACTS']    
-    #Copy results to build artifacts
-	  Dir.foreach("#{Dir.pwd}/test/tdriver_reports") do |entry|
-	    if entry.include?('test_run')
-	      FileUtils.cp_r "#{Dir.pwd}/test/tdriver_reports/#{entry}", "#{ENV['CC_BUILD_ARTIFACTS']}/#{entry}"
+  begin
+    Dir.chdir("test")
+    result=system( "cucumber features -f TDriverReport::CucumberReporter" )
+    raise "Feature tests failed" if (result != true) or ($? != 0)
+  ensure
+    Dir.chdir("..")
+    if ENV['CC_BUILD_ARTIFACTS']
+      #Copy results to build artifacts
+      Dir.foreach("#{Dir.pwd}/test/tdriver_reports") do |entry|
+        if entry.include?('test_run')
+          FileUtils.cp_r "#{Dir.pwd}/test/tdriver_reports/#{entry}", "#{ENV['CC_BUILD_ARTIFACTS']}/#{entry}"
           FileUtils::remove_entry_secure("#{Dir.pwd}/test/tdriver_reports/#{entry}", :force => true)
-	    end
-	  end
+        end
+      end
     end
-   end
-   exit(0)
-   
+  end
+  exit(0)
 end
 
 desc "Task for cruise control"
 task :cruise => ['build_visualizer', 'test'] do
-	exit(0)
+  exit(0)
 end
 
 desc "Task for building the example QT application(s)"
@@ -53,7 +51,7 @@ task :build_visualizer do
   puts "#########################################################"
 
   make = "make"
-  sudo = ""	
+  sudo = ""
 
   if /win/ =~ RUBY_PLATFORM || /mingw32/ =~ RUBY_PLATFORM
     make = "mingw32-make"
@@ -65,21 +63,17 @@ task :build_visualizer do
 
   cmd = "#{make} distclean"
   system(cmd)
-  
+
   cmd = "qmake CONFIG+=release"
   failure = system(cmd)
-  raise "qmake failed" if (failure != true) or ($? != 0) 
-    
+  raise "qmake failed" if (failure != true) or ($? != 0)
+
   cmd = "#{make}"
   failure = system(cmd)
-  raise "make release failed" if (failure != true) or ($? != 0) 
-    
+  raise "make release failed" if (failure != true) or ($? != 0)
+
   cmd = sudo + "#{make} install"
   failure = system(cmd)
-  raise "make install failed" if (failure != true) or ($? != 0) 
+  raise "make install failed" if (failure != true) or ($? != 0)
   puts "visualizer built"
 end
-
-
-
-
